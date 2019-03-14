7d4d4df74ba82ec3530dcc2590f2239cd5647006
Luke Williams <luke@turbox.uk>
2019-03-14 15:07:01 +0000

    Use pixel experience keydisabler

--- board/hw.mk
+++ board/hw.mk
@@ -1,8 +1,7 @@
 # Hardware
 
 JAVA_SOURCE_OVERLAYS := \
-    org.lineageos.hardware|hardware/samsung/lineagehw|**/*.java \
-    org.lineageos.hardware|device/samsung/hero-common/lineagehw|**/*.java
+    org.pixelexperience.keydisabler|device/samsung/hero-common/keydisabler|**/*.java
 
 # DT2W
 TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
